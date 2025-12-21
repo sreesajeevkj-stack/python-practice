cd5b98deb8976b14a580bf370f00191ac8589a0e
Sree Sajeev KJ <sreesajeevkj@gmail.com>
2025-12-21 16:54:09 +0530

    input.py

---
+++ input.py
@@ -0,0 +1,22 @@
+"""
+#AREA OF A RECTANGLE  1
+
+length = float(input("Enter the length of the rectangle:"))
+breath = float(input("Enter the breath of the rectangle:"))
+
+area = length * breath
+print(f"AREA OF A RECTANGLE IS : {area} cm²")
+"""
+
+"""
+#SHOPPING KART PROGRAM  2
+
+item =input("Which item whould you like to purchase??: ")
+price = float(input("What is the price??: "))
+quantity = int(input("How many would you like?: "))
+total = price*quantity
+
+print(f"You have bought {quantity} X {item}/s")
+print(f"Yopur total is : ${total}")
+
+"""

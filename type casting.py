c735602564dc304e46eac894665e06b4cfa32411
Sree Sajeev KJ <sreesajeevkj@gmail.com>
2025-12-21 16:49:51 +0530

    type casting

---
+++ type casting.py
@@ -0,0 +1,13 @@
+name = "sajeev"
+age = 24
+distance = 4.5
+graduate = True
+
+name  = bool(name)
+age =str(age)
+distance =int(distance)
+
+print (type(name))
+print (type(age))
+print (type(distance))
+print (graduate)

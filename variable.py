9ff3e8fb09371938ad5596797d5eb77fdd020178
Sree Sajeev KJ <sreesajeevkj@gmail.com>
2025-12-21 16:46:58 +0530

    variable basics

---
+++ variable.py
@@ -0,0 +1,12 @@
+user_name="Sajeev"
+year=2025
+pi=3.14
+is_admin=True
+
+print(f"welcome {user_name}!!!")
+print(f"present year : {year}")
+print(f"The value of pi : {pi}")
+if is_admin:
+    print("WELCOME TO THE PAGE!!!")
+else:
+    pprint("NEED ADMINS PERMISSION :( ")

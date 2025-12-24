013648e83c8154ea2cf4b0f66015fc9476b68b6e
sreesajeevkj-stack <sreesajeevkj@gmail.com>
2025-12-24 07:27:34 +0530

    Update temperature convertor.py

--- temperature convertor.py
+++ temperature convertor.py
@@ -2,11 +2,11 @@ unit = input("Enter the temperature wheather in celcius [C]or fahrenhiet [F] :")
 temperature = float(input("Enter the temperature:"))
 
 if unit == "C" :
-    temperature = (temperature * 9)/5 + 32
+    temperature = round((temperature * 9)/5 + 32 ,2)
     unit = "farenhiet"
     print(f"The converted temperature is {temperature} {unit}")
 elif unit == "F" :
-    temperature = (temperature -32)* 5/9
+    temperature = round((temperature -32)* 5/9 ,2)
     unit = "celcius"
     print(f"The converted temperature is {temperature} {unit}")
 else:

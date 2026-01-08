3aab916764e3fe5ba3f6efece09e5c36dc13febd
Sree Sajeev KJ <sreesajeevkj@gmail.com>
2026-01-08 08:25:56 +0530

    ternary operator

---
+++ TERNARY.py
@@ -0,0 +1,15 @@
+num=34
+a=5
+b=7
+age = 34
+Temperature = 30
+
+#print("POSITIVE" if num >0 else "NEGATIVE")
+#print("EVEN" if num%2 == 0 else "ODD")
+#maxvalue = (a if a > b else b)
+#minvalue = (a if a < b else b)
+#status = ("ADULT" if age>=18 else "CHILD")
+#print("ITS HOT OUTSIDE" if Temperature>25 else "THE TEMPERATURE IS NORMAL")
+
+
+#print(status)

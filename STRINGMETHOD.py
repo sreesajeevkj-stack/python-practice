a16e919d36ae2ab1fc5c069f28d0dd3f183a6f53
Sree Sajeev KJ <sreesajeevkj@gmail.com>
2026-01-08 20:31:36 +0530

    string methods

---
+++ STRINGMETHOD.py
@@ -0,0 +1,14 @@
+NAME = input("enter the name : ")
+
+NUMBER = int(input("enter the mobile number : "))
+
+#result = NAME.find("s")     //finds particular letter's location from the variable
+#result = NAME.rfind("s")    //finds particular letter's location from the variable in the reverse order
+#result = NAME.capitalize()  //used to capitalize the first letter of the input
+#result = NAME.upper()       //used to make every input velue as uppercase
+#result = NAME.lower()       //used to make every input velue as lowercase
+#result = NAME.isalpha()     //used to verify the value and confirms it as alphabet and display the result as true
+#result = NUMBER.count(" ")  //used to count the value mentioned within the quotes
+#result = NAME.replace(" ","_") //replace the value within the quotes
+
+print(result)
